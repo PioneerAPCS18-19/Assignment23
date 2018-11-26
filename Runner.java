84b99c2b1cb2d7d2d04b42b81ce17f693031fed0
codytheking <codytheking93@gmail.com>
2018-11-26 12:18:11 -0800

    Starter code

---
+++ Runner.java
@@ -0,0 +1,31 @@
+/**
+ * Arrays
+ *
+ * @author
+ * @date 11/26/2018
+ */
+
+import java.util.Scanner;
+
+public class Runner
+{
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+
+
+        // create an array of ints with the identifier "values"
+        
+        
+        for(int i = 0; i < values.length; i++)
+        {
+            System.out.print("Enter value: ");
+            values[i] = in.nextInt();
+        }
+    }
+}
+
+// maxVal
+// @params array of ints
+// @return largest int value
+
